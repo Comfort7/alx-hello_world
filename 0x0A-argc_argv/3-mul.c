8f9d3be27f44375a5a160810f4e9f2ca9a198a27
Comfort7 <comfortehachi07@gmail.com>
2023-09-02 03:16:34 -0700

    task 4

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,35 @@
+/*
+ * File: multiply.c
+ * Author: Your Name
+ * Description: A C program that multiplies two numbers and prints the result.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point of the program.
+ * @argc: The number of command line arguments.
+ * @argv: An array containing the command line arguments.
+ *
+ * Return: 0 if successful, 1 if there are not enough arguments.
+ */
+int main(int argc, char *argv[])
+{
+	int num1, num2, result;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+
+	result = num1 * num2;
+
+	printf("%d\n", result);
+
+	return (0);
+}
