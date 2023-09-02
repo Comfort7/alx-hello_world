14f571bd2f79af7b255b9567d7b2c2a3b6afeb97
Comfort7 <comfortehachi07@gmail.com>
2023-09-02 02:57:27 -0700

    task 1

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,17 +1,33 @@
-#include <stdio.h>
-#include <libgen.h>
-
-/**
- * main - Entry point of the program
- *
- * Return: Always 0
+/*
+ * File: print_program_name.c
+ * Author: Your Name
+ * Description: A C program that prints its name, including the path.
  */
 
-int main(void)
-{
-	char *mynameis = basename(__FILE__);
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-	printf("%s\n", mynameis);
+/**
+ * main - Entry point of the program.
+ * @argc: The number of command line arguments.
+ * @argv: An array containing the command line arguments.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	if (argc > 0 && argv[0])
+	{
+	char *program_name = argv[0];
+
+	printf("%s\n", program_name);
+	}
+	else
+	{
+		fprintf(stderr, "Error: Unable to retrieve program name.\n");
+		return (1);
+	}
 
 	return (0);
 }
