dea62b36b66f39a081367fe4c42cff859bbe5e48
Comfort7 <comfortehachi07@gmail.com>
2023-09-11 21:10:09 -0700

    task 1

---
+++ 0x0D-preprocessor/1-pi.h
@@ -0,0 +1,14 @@
+#ifndef MY_MATH_H
+#define MY_MATH_H
+
+/**
+ * PI - A macro that represents the mathematical constant Pi (π).
+ *
+ * This macro is defined to simplify the use of the value of Pi
+ * throughout the program. You can use it by simply typing PI
+ * instead of the numeric value 3.14159265359.
+ */
+
+#define PI 3.14159265359
+
+#endif
