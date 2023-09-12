aec58a488b568097b0999bf0e0ed035087ef262e
Comfort7 <comfortehachi07@gmail.com>
2023-09-11 21:17:51 -0700

    task 4

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,18 @@
+#ifndef SUM_MACRO_H
+#define SUM_MACRO_H
+
+/**
+ * SUM - A macro that computes the sum of two numbers.
+ *
+ * Description: This macro takes two numeric arguments, `x` and `y`,
+ * and computes their sum. It returns the sum of `x` and `y`.
+ *
+ * @x: The first number to be added.
+ * @y: The second number to be added.
+ *
+ * Return: The sum of the input numbers `x` and `y`.
+ */
+
+#define SUM(x, y) ((x) + (y))
+
+#endif
