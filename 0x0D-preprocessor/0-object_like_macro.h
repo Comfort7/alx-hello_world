8f28e822669caac7bc852a88ffcde292017eef8f
Comfort7 <comfortehachi07@gmail.com>
2023-09-11 21:06:08 -0700

    task 0

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,14 @@
+#ifndef MY_HEADER_H
+#define MY_HEADER_H
+
+/**
+ * SIZE - A macro that represents the value 1024.
+ *
+ * This macro is defined to simplify the use of the value 1024
+ * throughout the program. You can use it by simply typing SIZE
+ * instead of the numeric value.
+ */
+
+#define SIZE 1024
+
+#endif
